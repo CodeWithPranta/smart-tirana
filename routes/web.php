d25d892f80c720a1820696752916356a37869fca
webapp99 <codewithpranta@gmail.com>
2024-06-07 23:32:37 +0600

    update 7 jun - 1

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('landing');
-});
+Route::get('/', [LandingPageController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
